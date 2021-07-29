e7f4a5608f179a07ef1c997684764481fa4d72a7
Erjanazimov <erjan.azimov@gmail.com>
2021-07-29 21:39:39 +0600

    file2

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+
+	Array
+
+	slice ( start: int, end: int )
+
+	Return Type:
+	Array
+
+	Description:
+	The slice method takes two arguments, start and end, and returns an array containing the elements of the array from element start up to, but not including, element end (or through the end of the array if end is undefined). If start is negative, it is treated as length+start where length is the length of the array.
+
+	URL doc:
+	http://html5index.org/ECMAScript%20-%20Array.html#slice
+
+*/
